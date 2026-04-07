c3f806af44c10a127af4bd1b711150433637a14d
Manvir Singh <102511754+Manilehra09@users.noreply.github.com>
2026-04-07 12:26:18 +0530

    Add file.sh to the repository

---
+++ file.sh
@@ -0,0 +1,20 @@
+# Create a container file
+cat <<EQF > Containerfile
+FROM docker.io/amazonlinux:latest
+RUN yum install httpd -y 
+RUN echo "This webserver is craeted using jenkins" >> /var/www/html/index.html
+EXPOSE 80
+CMD "/usr/sbin/httpd" -D "FOREGROUND"
+EQF
+
+# Build a container image from file 
+sudo podman build -t httpd:latest -f Containerfile .
+
+# Verify the image 
+sudo podman images 
+
+# Run container from custome image 
+sudo podman run -itd -p 8002:80 httpd:latest
+
+# Verify Container 
+sudo podman ps -a 
