830eb6391879a37319ccab2b21c8aef9e3b5a0f5
Manvir Singh <102511754+Manilehra09@users.noreply.github.com>
2026-04-09 10:10:10 +0530

    v1

--- file.sh
+++ file.sh
@@ -4,14 +4,15 @@ set -e  # This tells the script to exit immediately if any command fails
 cat <<EOF > Containerfile
 FROM docker.io/amazonlinux:latest
 RUN yum install httpd -y 
-RUN echo "This webserver is created using jenkins" >> /var/www/html/index.html
+COPY ./src/ /var/www/html/
+RUN chown -R apache:apache /var/www/html
 EXPOSE 80
 CMD ["/usr/sbin/httpd", "-D", "FOREGROUND"]
 EOF
 
 # 2. Build the image
 # Using sudo if required, or just podman if permissions are set
-sudo podman build -t httpd:latest -f Containerfile .
+sudo podman build -t LMS:latest -f Containerfile .
 
 # 3. CLEANUP: Remove old container if it exists
 echo "Cleaning up old containers..."
@@ -20,7 +21,7 @@ sudo podman rm lms-webserver || true
 
 # 4. Run new container
 echo "Starting new container..."
-sudo podman run -d --name lms-webserver -p 8003:80 httpd:latest
+sudo podman run -d --name lms-webserver -p 8003:80 LMS:latest
 
 # 5. Verify
 sudo podman ps -a
